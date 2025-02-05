db7f90b849ceed67e0d06ac9cb133297135e5a60
Your Name <kim946509@gmail.com>
2025-02-05 22:39:22 +0900

    feat : 관심 상품에 폴더 추가 및 폴더 전체 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductFolerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.myselectshop.repository;
-
-import com.sparta.myselectshop.entity.ProductFolder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductFolerRepository extends JpaRepository<ProductFolder, Long> {
-}
